afc29dc315f040447f18929db4079ea55b7204da
Lea <nsane4stargate@gmail.com>
2022-02-11 20:04:34 -0700

    First Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen( 3524, () => {
+    console.log('Listening on container port: 3524');
+});
